f6a9f0d7de03430987aee32a6b873f77d9ff8f49
vivii14 <vineshdu92@gmail.com>
2024-05-30 07:15:59 +0000

    Implementation des fonctions pays ( Troles Thomas ♥)

--- JO/src/Package/Pays.java
+++ JO/src/Package/Pays.java
@@ -11,52 +11,91 @@ public class Pays {
 	private String Nom;
 
 	public Pays() {
-		// TODO - implement Pays.Pays
+		// TODO - implement Pays.Pay
+		this.nom = nom;
 		throw new UnsupportedOperationException();
 	}
 
 	public void afficherPays() {
 		// TODO - implement Pays.afficherPays
+		System.out.println("Nom du pays : " + nom);
 		throw new UnsupportedOperationException();
 	}
 
-	public void supprimerSportif() {
-		// TODO - implement Sportif.supprimerSportif
-		throw new UnsupportedOperationException();
+	public void supprimerSportif(Sportif s){
+		 if((sesSportifs.contains(s))){
+            sesSportifs.remove(s);
+        }else{
+            System.out.println("Le résultat n'existe pas pour cette session");
+        }
 	}
+	
 
 	public void modifierSportif() {
 		// TODO - implement Sportif.modifierSportif
 		throw new UnsupportedOperationException();
 	}
 
-	public void ajouterSportif() {
+	public void ajouterSportif(Sportif s){
 		// TODO - implement Sportif.supprimerSportif
-		throw new UnsupportedOperationException();
+		 if(!(sesSportifs.contains(s))){
+            sesSportifs.add(s);
+        }else{
+            System.out.println("Le sportif est déjà présent dans ce pays");
+        }
 	}
 
-	public void ajouterVille(){
+	public void ajouterVille(Ville v){
+		if(!(sesVilles.contains(v))){
+            sesVilles.add(v);
+        }else{
+            System.out.println("La ville est déjà présente dans ce pays");
+        }
 
 	}
 
-	public void supprimerVille(){
+	public void supprimerVille(Ville v){
+		if((sesVilles.contains(v))){
+            sesVilles.remove(v);
+        }else{
+            System.out.println("La ville n'existe pas pour ce pays");
+        }
 		
 	}
 
-	public void ajouterResultat(){
+	public void ajouterResultat(Resultat r){
+		if(!(sesResultats.contains(r))){
+            sesResultats.add(r);
+        }else{
+            System.out.println("Le Relsultat est déjà présente dans ce pays");
+        }
 
 	}
 
-	public void supprimerResultat(){
+	public void supprimerResultat(Resultat r){
+		if((sesResultats.contains(r))){
+            sesResultats.remove(r);
+        }else{
+            System.out.println("Le Resultat n'existe pas pour ce pays");
+        }
 		
 	}
 
-	public void ajouterEquipe(){
+	public void ajouterEquipe(Equipe e){
+		if(!(lEquipe.contains(e))){
+            lEquipe.add(e);
+        }else{
+            System.out.println("L'equipe est déjà présente dans ce pays");
+        }
 
 	}
 
-	public void supprimerEquipe(){
-		
+	public void supprimerEquipe(Equipe e){
+		if((lEquipe.contains(e))){
+            lEquipe.remove(e);
+        }else{
+            System.out.println("L'equipe n'existe pas pour ce pays");
+        }
 	}
 	
 	void modifierNomPays(){
