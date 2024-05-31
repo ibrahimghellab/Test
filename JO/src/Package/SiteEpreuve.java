db03c4be2ffdf34c160659b4282bb76207aeb784
Dede782 <dede78lept@gmail.com>
2024-05-31 07:36:43 +0000

    SiteEpreuve(Fini normalement)

--- JO/src/Package/SiteEpreuve.java
+++ JO/src/Package/SiteEpreuve.java
@@ -15,14 +15,13 @@ public class SiteEpreuve {
 	public SiteEpreuve(String nomSiteEpreuve, Ville saVille) {
 		this.nomSiteEpreuve=nomSiteEpreuve;
 		this.saVille=saVille;		
-		// TODO - implement SiteEpreuve.SiteEpreuve
+		
 		throw new UnsupportedOperationException();
 	}
 
 	public void afficherSiteEpreuve() {
-		System.out.println("L'épreuve se passe dans " + this.nomSiteEpreuve);
+		System.out.println("L'épreuve se passe au " + this.nomSiteEpreuve "dans la ville de "+saVille);
 		
-		// TODO - implement SiteEpreuve.afficherSiteEpreuve
 		throw new UnsupportedOperationException();
 	}
 
@@ -33,16 +32,29 @@ public class SiteEpreuve {
 	public void modifierNomSiteEpreuve(String nouveauNomSiteEpreuve) {
 		this.nomSiteEpreuve =nouveauNomSiteEpreuve;
 
-		// TODO - implement SiteEpreuve.modifierNomSiteEpreuve
 		throw new UnsupportedOperationException();
 	}
 
-		public void ajouterEpreuve(){
-
+		public void ajouterEpreuve(Epreuve epreuve){
+		if(!(lEpreuve.contains(epreuve)))
+		{
+        	lEpreuve.add(epreuve);
+        }
+		else
+		{           
+            System.out.println("L'épreuve que vous souhaitez ajouter est dejà présente");
+        }
 	}
 
-	public void supprimerEpreuve(){
-		
+	public void supprimerEpreuve(Epreuve epreuve){
+		if(lEpreuve.contains(epreuve))
+		{
+            lEpreuve.remove(epreuve);
+        }
+		else
+		{
+            System.out.println("L'épreuve que vous souhaitez supprimer n'existe pas");
+        }
 	}
 
 	public void modifierVille(Ville nouvelleVille){
